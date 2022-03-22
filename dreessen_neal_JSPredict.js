9118c58514ebc0d4ad1ddd968b93cd6265322470
dreessen-n <dreessen.edu@gmail.com>
2022-03-22 17:39:21 -0600

    Answer predictions to code challenge for javaScript

--- dreessen_neal_JSPredict.js
+++ dreessen_neal_JSPredict.js
@@ -1,2 +1,10 @@
 // Intro to JavaScript: Code Challenge: Predict Functions
 
+// predict 1: error value not defined
+
+// predict 2: "I was born in 1980"
+
+// predict 3: "Summing Numbers!"
+// "num1 is 10"
+// "num2 is 20"
+// 30

0e697ad5ac1f807fe04a1e78ba9c4a430c7a3c66
dreessen-n <dreessen.edu@gmail.com>
2022-03-23 15:08:09 -0600

    First commit of tinkering.. not sure how to test

---
+++ dreessen_neal_JS_Tinkering.js
@@ -0,0 +1,15 @@
+// Optional Code Challenge: JavaScript
+
+// Tall enough to ride the rollercoaster?
+
+var childHeight = 54;
+
+function displayIfChildIsAbleToRideTheRollerCoaster() {
+    if (childHeight > 52) {
+        console.log("Get on that ride, kiddo!")
+    } else {
+        console.log("Sorry kiddo. Maybe next year.")
+    }
+
+}
+

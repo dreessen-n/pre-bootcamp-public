70393f78a9c6fdfc026cee582b37e5a8c6c7be25
dreessen-n <dreessen.edu@gmail.com>
2022-03-22 19:45:35 -0600

    Update the js code callenge. fixed mistakes

--- dreessen_neal_JSPredict.js
+++ dreessen_neal_JSPredict.js
@@ -1,10 +1,36 @@
 // Intro to JavaScript: Code Challenge: Predict Functions
+// I am updating this file after reviewing the solution video
+// I wasn't sure if I was to copy the code in or just the comment answers
+// so adding in the code.
+// also I got the first one wrong. I wasn't sure if you could concatenate
+// a string and an integer.
 
-// predict 1: error value not defined
+function myBirthYearFunc(){
+        console.log("I was born in " + 1980);
+    }
+    
+// predict 1: 
+// I was  born in 1980
 
-// predict 2: "I was born in 1980"
+// input is 1980
+function myBirthYearFunc(birthYearInput){
+        console.log("I was born in " + birthYearInput);
+    }
+    
+// predict 2: 
+// I was born in 1980
 
-// predict 3: "Summing Numbers!"
-// "num1 is 10"
-// "num2 is 20"
+// num1 = 10, num2 = 20
+function add(num1, num2){
+        console.log("Summing Numbers!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+
+// predict 3: 
+// Summing Numbers!
+// num1 is 10
+// num2 is 20
 // 30

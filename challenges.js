2a9ddc5baa96d16b483813da0eabeefc81bbd57b
dreessen-n <dreessen.edu@gmail.com>
2022-03-31 14:33:17 -0600

    Add challenge 6 to file and still neeed to work out what's wrong with for loop... keeps ending after one iteration

--- challenges.js
+++ challenges.js
@@ -1,11 +1,31 @@
+// challenge 5:
+// var max;
+// function findMax(arr) {
+//     var max;
+//     max = Number(Math.max(arr));
+//     return max;
+// }
+
+// findMax([300,2,200,5])
+
+// console.log(max);
 
 
-function findMax(arr) {
-    var max;
-    max = Math.max(arr);
-    return max;
+// Challenge 6: something wrong with my for loop!!
+function findAve (arr) {
+    console.log(arr)
+    console.log(arr.length)
+    var i = 0;
+    var total = 0;
+    for (i = 0; i < arr.length;) {
+        total = total + arr[i];
+        console.log(i);
+        console.log(total);
+        i += 1;
+    var ave = total / arr.length;
+    return ave;
+    }
+
 }
-
-findMax([300,2,200,5])
-
-console.log(max);
+ 
+findAve([1, 3, 5, 7, 20])

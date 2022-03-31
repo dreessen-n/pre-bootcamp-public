6466646f6dc3454a88e0c9b3502778c74333f56a
dreessen-n <dreessen.edu@gmail.com>
2022-03-31 15:16:33 -0600

    Update to challenge 5

--- challenges.js
+++ challenges.js
@@ -1,24 +1,31 @@
 // challenge 5:
-// var max;
-// function findMax(arr) {
-//     var max;
-//     max = Number(Math.max(arr));
-//     return max;
-// }
+function findMax(arr) {
+    var max = arr[0];
+    console.log(max);
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+            console.log(max);
+        }
+    }
 
-// findMax([300,2,200,5])
 
-// console.log(max);
+    return max;
+}
+
+findMax([300,2,200,5]);
+
+console.log(max);
 
 
 // Challenge 6: something wrong with my for loop!!
-function findAve (arr) {
-    var total = 0;
-    for ( var i = 0; i < arr.length; i++) {
-        total = total + arr[i];
-    }
-    var avg = total / arr.length;
-    return avg;
-    }
+// function findAve (arr) {
+//     var total = 0;
+//     for ( var i = 0; i < arr.length; i++) {
+//         total = total + arr[i];
+//     }
+//     var avg = total / arr.length;
+//     return avg;
+//     }
  
-findAve([1, 3, 5, 7, 20])
+// findAve([1, 3, 5, 7, 20])

1e442962daa462e0a842f5080a0768ace51a1d4b
dreessen-n <dreessen.edu@gmail.com>
2022-04-01 15:19:14 -0600

    Add challenge 7

--- challenges.js
+++ challenges.js
@@ -1,19 +1,19 @@
 // challenge 5:
-function findMax(arr) {
-    var max = arr[0];
+// function findMax(arr) {
+//     var max = arr[0];
     
-    for (var i = 1; i < arr.length; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
+//     for (var i = 1; i < arr.length; i++) {
+//         if (arr[i] > max) {
+//             max = arr[i];
             
-        }
-    }
+//         }
+//     }
 
-    console.log(max);
-    return max;
-}
+//     console.log(max);
+//     return max;
+// }
 
-findMax([300,2,200,5]);
+// findMax([300,2,200,5]);
 
 
 
@@ -29,3 +29,21 @@ findMax([300,2,200,5]);
 //     }
  
 // findAve([1, 3, 5, 7, 20])
+
+
+// Challenge 7: 
+
+// function oddNumbers() {
+//     var arr = [];
+//     for (var i = 1; i < 50;) {
+//         arr.push(i);
+//         i = i + 2;
+//         console.log(arr)
+//     }
+    
+//     return arr;
+// }
+
+// oddNumbers();
+
+// Challenge 8:
